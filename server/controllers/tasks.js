9bda3855110707e536ec5603a4fc47d319c6ffcb
Jennifer Wickersham <jennifermwickersham@gmail.com>
2018-05-03 20:46:04 -0700

    create additional task routes for post, put and delete

--- server/controllers/tasks.js
+++ server/controllers/tasks.js
@@ -8,26 +8,84 @@ let Task = require("../models/task");
 // Routes
 router.get("/", function (req, res, next) {
     Task.find({}, function (err, allTasks) {
-      if (err) {
-        console.log(err);
-      } else {
-        httpStatus = err ? 400 : 200;
+        if (err) {
+            console.log(err);
+            httpStatus = 400;
+        }
+        else {
+            httpStatus = 200;
+        }
         return res.status(httpStatus).json(allTasks);
-      }
     });
-  });
-  
-  // Show route - Shows more info about one task
-  router.get("/:id", function (req, res, next) {
+});
+
+// Show route - Shows more info about one task
+router.get("/:id", function (req, res, next) {
     // Find the task with provided ID
     Task.findById(req.params.id, function (err, foundTask) {
-      if (err || !foundTask) {
-        console.log(err);
-      } else {
-        httpStatus = err ? 400 : 200;
+        if (err) {
+            console.log(err);
+            httpStatus = 400;
+        } 
+        else if (!foundTask) {
+            httpStatus = 404;
+        }
+        else {
+            httpStatus = 200;
+        }
         return res.status(httpStatus).json(foundTask);
-      }
     });
-  });
+});
+
+// Create route - Add a new task to DB
+router.post("/", function (req, res, next) {
+    let newTask = {
+        short_description: req.body.short_description,
+        description: req.body.description
+    };
+
+    Task.create(newTask, function (err, newTask) {
+        if (err) {
+            console.log(err);
+            httpStatus = 400;
+        } 
+        else {
+            httpStatus = 201;
+        }
+        return res.status(httpStatus).json(newTask);
+    });
+});
+
+// Update route
+router.put("/:id", function(req, res) {
+    // Find and update correct task
+    Task.findByIdAndUpdate(req.params.id, req.body.task, function(err, updatedTask) {
+        if (err || !updatedTask) {
+            console.log(err);
+            httpStatus = 400;
+        }
+        else if (!updatedTask) {
+            httpStatus = 404;
+        }
+        else {
+            httpStatus = 201;
+        }
+        return res.status(httpStatus).json(updatedTask);
+    });
+});
+
+// Destroy Route
+router.delete("/:id", function(req, res) {
+    Task.findByIdAndRemove(req.params.id, function(err) {
+        if (err) {
+            console.log(err);
+            httpStatus = 400;
+        }
+        else {
+            httpStatus = 204;
+        }
+        return res.status(httpStatus);
+    });
+});
 
 module.exports = router;
