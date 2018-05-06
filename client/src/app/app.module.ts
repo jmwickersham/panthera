51d4359eac5267de273eba9102076a64fece8e02
Jennifer Wickersham <jennifermwickersham@gmail.com>
2018-05-06 02:33:28 -0700

    moved node_modules up to root directory

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,25 +5,24 @@ import { BrowserModule }     from '@angular/platform-browser';
 import { FormsModule }       from '@angular/forms';
 import { HttpClientModule }  from '@angular/common/http';
 
-import { AppComponent }      from './app.component';
-import { MessagesComponent } from './messages/messages.component';
-import { TasksComponent }    from './tasks/tasks.component';
+import { AppComponent }        from './app.component';
+import { MessagesComponent }   from './messages/messages.component';
+import { TasksComponent }      from './tasks/tasks.component';
 import { TaskDetailComponent } from './task-detail/task-detail.component';
-import { UsersComponent } from './users/users.component';
+import { UsersComponent }      from './users/users.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { LoginComponent }      from './login/login.component';
+import { RegisterComponent }   from './register/register.component';
 
-import { TaskService }       from './services/task.service';
-import { MessageService }    from './services/message.service';
-import { UserService }       from './services/user.service';
+import { TaskService }           from './services/task.service';
+import { MessageService }        from './services/message.service';
+import { UserService }           from './services/user.service';
 import { AuthenticationService } from './services/authentication.service';
-import { AuthGuardService } from './services/auth-guard.service';
+import { AuthGuardService }      from './services/auth-guard.service';
 
-import { AppRoutingModule }  from './app-routing.module';
+import { AppRoutingModule }   from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProfileComponent } from './profile/profile.component';
-
+import { ProfileComponent }   from './profile/profile.component';
 
 @NgModule({
   imports: [
