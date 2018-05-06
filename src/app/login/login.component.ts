6ae6359084a249702b92f26736a3b1752f25c01a
Jennifer Wickersham <jennifermwickersham@gmail.com>
2018-05-05 20:12:05 -0700

    update login compenent

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient }        from '@angular/common/http';
+import { Router }            from "@angular/router";
+import { Observable }        from 'rxjs/Observable';
+import { tap, catchError }   from 'rxjs/operators';
+import { of }                from 'rxjs/observable/of';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginData = { username: '', password: '' };
+  message = '';
+  data: any;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient, 
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  login() {
+    this.http.post('/api/login', this.loginData)
+      .subscribe(resp => {
+        this.data = resp;
+        localStorage.setItem('jwtToken', this.data.toke);
+      }, err => {
+        this.message = err.error.msg;
+      }
+    );
+  }
 }
