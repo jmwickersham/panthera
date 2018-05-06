273c28e9d6cb910276932f39e92d26ab604b135d
Jennifer Wickersham <jennifermwickersham@gmail.com>
2018-05-05 20:27:26 -0700

    update register compenent

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient }        from '@angular/common/http';
-import { Router }            from "@angular/router";
+import { Router }            from '@angular/router';
 import { Observable }        from 'rxjs/Observable';
 import { tap, catchError }   from 'rxjs/operators';
 import { of }                from 'rxjs/observable/of';
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient }        from '@angular/common/http';
+import { Router }            from '@angular/router';
+import { Observable }        from 'rxjs/Observable';
+import { tap, catchError }   from 'rxjs/operators';
+import { of }                from 'rxjs/observable/of';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +11,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerData = { username: '', password: '' };
+  message = '';
 
-  constructor() { }
+  constructor(
+    private http: HttpClient, 
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  register() {
+    this.http.post('/api/register', this.registerData)
+      .subscribe(resp => {
+        console.log(resp);
+        this.router.navigate(['login']);
+      }, err => {
+        this.message = err.error.msg;
+      });
+  }
 }
