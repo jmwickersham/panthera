bfd91af2b42e1f396590d6f4c3f36bfbefd94dec
Jennifer Wickersham <jennifermwickersham@gmail.com>
2018-05-06 22:08:01 -0700

    shortened server.js code

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { environment } from '../environments/environment';
 
+// Modules
 import { NgModule }          from '@angular/core';
 import { BrowserModule }     from '@angular/platform-browser';
 import { FormsModule }       from '@angular/forms';
 import { HttpClientModule }  from '@angular/common/http';
+import { AppRoutingModule }  from './app-routing.module';
 
+// Components
 import { AppComponent }        from './app.component';
 import { MessagesComponent }   from './messages/messages.component';
 import { TasksComponent }      from './tasks/tasks.component';
@@ -13,16 +16,15 @@ import { UsersComponent }      from './users/users.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
 import { LoginComponent }      from './login/login.component';
 import { RegisterComponent }   from './register/register.component';
+import { DashboardComponent }  from './dashboard/dashboard.component';
+import { ProfileComponent }    from './profile/profile.component';
 
+// Services
 import { TaskService }           from './services/task.service';
 import { MessageService }        from './services/message.service';
 import { UserService }           from './services/user.service';
 import { AuthenticationService } from './services/authentication.service';
 import { AuthGuardService }      from './services/auth-guard.service';
-
-import { AppRoutingModule }   from './app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProfileComponent }   from './profile/profile.component';
 
 @NgModule({
   imports: [
---
+++ server.js
@@ -0,0 +1,59 @@
+// Require Packages
+const express       = require("express"),
+      bodyParser    = require("body-parser"),
+      passport      = require("passport"),
+      favicon       = require("serve-favicon"),
+      cookieParser  = require('cookie-parser'),
+      cors          = require('cors');
+
+// Bring in mongoose data model
+require('./api/models/database');
+
+// Bring in passport config
+require('./api/config/passport');
+
+// Require JS Controller Exports
+let taskRoutes  = require("./api/routes/tasks"),
+    userRoutes  = require("./api/routes/users"),
+    indexRoutes = require("./api/routes/index");
+
+// Create link to Angular build directory
+let distDir = __dirname + "/dist/";
+
+// Set up App
+let app = express();
+app.use(favicon(__dirname + '/client/src/favicon.ico'));
+app.use(express.static(distDir));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( {extended: false} ));
+app.use(cookieParser());
+app.use(cors());
+
+// Passport Config
+app.use(passport.initialize());
+
+// Route config
+app.use("/", indexRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/users", userRoutes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Catch unauthorized errors
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401);
+    res.json({"message" : err.name + ": " + err.message});
+  }
+});
+
+// Initialize app
+let server = app.listen(process.env.PORT || 3000, function () {
+  let port = server.address().port;
+  console.log("Panthera server has started on port", port);
+});
