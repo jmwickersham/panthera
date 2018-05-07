44481509615997f488d83257b83c98211cb90544
Jennifer Wickersham <jennifermwickersham@gmail.com>
2018-05-06 20:16:15 -0700

    added function to normalize port

--- app.js
+++ app.js
@@ -65,9 +65,24 @@ app.use(function (err, req, res, next) {
   }
 });
 
-let port = parseInt(process.env.PORT) || 3000;
+let port = normalizePort(process.env.PORT || 3000);
 
-let server = app.listen(port, function () {
-  let port = server.address().port;
+app.listen(port, function () {
   console.log("Panthera server has started on port", port);
 });
+
+function normalizePort(val) {
+  let port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
