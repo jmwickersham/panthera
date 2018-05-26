19035fe0c1bd78af10f7e63401f3fd5c007d192c
Jennifer Wickersham <jennifermwickersham@gmail.com>
2018-05-25 20:45:13 -0700

    add status and category placeholders to task schema

--- api/models/task.js
+++ api/models/task.js
@@ -3,6 +3,14 @@ const mongoose = require("mongoose");
 const taskSchema = new mongoose.Schema({
     short_description: String,
     description: String,
+    status: {
+        type: String,
+        enum: ['New','In Progress','Blocked','On Hold','Complete']
+    },
+    category: {
+        type: String,
+        enum: ['Category1','Category2','Category3']
+    },
     comments: [
         {
             type: mongoose.Schema.Types.ObjectId,
