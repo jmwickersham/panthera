287d219cb12e794cfd3d35b283ae65837b0e627e
Jennifer Wickersham <jennifermwickersham@gmail.com>
2018-07-11 20:33:59 -0700

    Temporary fix for Login and Register Routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: 'landing', loadChildren: 'app/landing/landing.module#LandingModule' },
   { path: 'tasks', loadChildren: 'app/tasks/task.module#TaskModule' },
   { path: 'users', loadChildren: 'app/users/user.module#UserModule' },
+  { path: 'login', loadChildren: 'app/login/login.module#LoginModule' },
   { path: '', redirectTo: '/landing', pathMatch: 'full' }
 ];
 
